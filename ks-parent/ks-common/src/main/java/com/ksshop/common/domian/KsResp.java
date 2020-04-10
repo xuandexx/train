c1f4fcda8c43ad8bb17fd0472acd0fb8df9e5e9e
Simon <Simon@DESKTOP-31A5EN6>
2020-04-10 10:13:58 +0800

    A

---
+++ ks-parent/ks-common/src/main/java/com/ksshop/common/domian/KsResp.java
@@ -0,0 +1,26 @@
+package com.ksshop.common.domian;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class KsResp<T> {
+
+	private Integer code;
+
+	private String msg;
+
+	private T data;
+
+	public static <T> KsResp<T> fail(Integer code, String msg) {
+		return new KsResp<T>(code, msg, null);
+	}
+
+	public static <T> KsResp<T> success(Integer code, T data) {
+		return new KsResp<T>(code, null, data);
+	}
+
+}
