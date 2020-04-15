2aa32c306af65e6a838dff8f24de95f335c7b3f1
Simon <289802612@qq.com>
2020-04-15 16:29:31 +0800

    A

---
+++ Android/app/src/main/java/com/xuandexx/android/train/base/BaseApplication.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (C) 2016 Hyphenate Inc. All rights reserved.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.xuandexx.android.train.base;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * 应用程序入口
+ */
+public class BaseApplication extends Application {
+
+    public static Context applicationContext;
+
+    private static BaseApplication instance;
+
+    public final String userToken = "username";
+
+    /**
+     * nickname for current user, the nickname instead of ID be shown when user receive notification from APNs
+     */
+    public static String currentUserNick = "";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        applicationContext = this;
+        instance = this;
+    }
+
+    public static BaseApplication getInstance() {
+        return instance;
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+    }
+}
